becf82d3675df83f91e0b7439e54db796b202f5f
jgjgill <dbdltm22@naver.com>
2023-12-05 00:56:51 +0900

    first commit

---
+++ theme.config.jsx
@@ -0,0 +1,11 @@
+export default {
+  logo: <span>Concept Note</span>,
+  project: {
+    link: "https://github.com/jgjgill",
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s",
+    };
+  },
+};
